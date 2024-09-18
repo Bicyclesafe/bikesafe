a3e9a6c5a31164b66bac79681af78212621a3ca7
Tommi Juslin <tommi.juslin@gmail.com>
2024-09-18 13:27:54 +0300

    use env variable for api

--- client/src/services/pinService.tsx
+++ client/src/services/pinService.tsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { apiUrl } from "../util/config"
 
 export const getAll = async () => {
-  return await axios.get('http://localhost:3000/api/coordinates')
+  return await axios.get(`${apiUrl}/api/coordinates`)
 }
 
 export default { getAll }
--- client/src/services/theftService.tsx
+++ client/src/services/theftService.tsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { apiUrl } from "../util/config"
 import { LatLng } from "leaflet"
 
 export const sendTheftReport = async (coordinates: LatLng) => {
-  const res = await axios.post('http://localhost:3000/api/coordinates', coordinates)
+  const res = await axios.post(`${apiUrl}/api/coordinates`, coordinates)
   return res.data
 }
