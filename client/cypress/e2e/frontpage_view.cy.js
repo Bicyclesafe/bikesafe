26ef040d6c69cc1b8fe08a814cdc961171d6f08f
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-09-18 10:31:11 +0300

    Modify cypress tests

--- client/cypress/e2e/frontpage_view.cy.js
+++ client/cypress/e2e/frontpage_view.cy.js
@@ -1,11 +1,19 @@
-describe('Frontpage', () => {
+const { Marker } = require("react-leaflet")
+
+describe('Frontpage-tests:', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173')
   })
-  it('can be opened', () => {
+  it('Frontpage can be opened', () => {
     cy.contains('Report theft')
   })
-  it('contains Map element',() => {
+  it('Frontpage contains Map element',() => {
     cy.get('.leaflet-container').should('exist')
+  })
+  it('Marker can be placed on the map', () => {
+    cy.contains('Report theft').click()
+    cy.get('.leaflet-container').should('exist').click(500,300)
+    cy.contains('Confirm').click()
+    
   })
 })
