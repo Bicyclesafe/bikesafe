8ea7d3f9648568733997d8445d0201d4a9f8c123
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-09-18 09:51:56 +0300

    Add frontpage testing for cypress

--- client/cypress/e2e/frontpage_view.cy.js
+++ client/cypress/e2e/frontpage_view.cy.js
@@ -1,6 +1,11 @@
 describe('Frontpage', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173')
+  })
+  it('can be opened', () => {
     cy.contains('Report theft')
+  })
+  it('contains Map element',() => {
+    cy.get('.leaflet-container').should('exist')
   })
 })
