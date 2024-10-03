8a208f7f5b7dcb2cba6ee123bdca6ff3820472ab
Tommi Juslin <tommi.juslin@gmail.com>
2024-10-03 17:56:44 +0300

    refactor ReportModal component

--- client/src/components/ReportModal.tsx
+++ client/src/components/ReportModal.tsx
@@ -5,19 +5,29 @@ import { sendTheftReport } from "../services/theftService"
 import styles from './ReportModal.module.css'
 import appStyles from '../App.module.css'
 
-const ReportModal: FC<ReportModalProps> = ({ theftPosition, setTheftPosition, bikeThefts, setBikeThefts }) => {
-    const handleReportConfirm = async (position: LatLng) => {
-        const newMarker = await sendTheftReport(position)
-        console.log(position)
-        setBikeThefts(bikeThefts.concat(newMarker))
-        setTheftPosition(null)
-    }
+const ReportModal: FC<ReportModalProps> = ({
+  theftPosition,
+  setTheftPosition,
+  bikeThefts,
+  setBikeThefts
+}) => {
+  const handleReportConfirm = async (position: LatLng) => {
+    const newMarker = await sendTheftReport(position)
+    console.log(position)
+    setBikeThefts(bikeThefts.concat(newMarker))
+    setTheftPosition(null)
+  }
 
-    return theftPosition && (
-        <div>
-            <button className={appStyles['base-button']} id={styles['confirm-button']} onClick={() => handleReportConfirm(theftPosition)}>Confirm</button>
-        </div>
-    )
+  return theftPosition && (
+    <div>
+      <button
+        className={appStyles['base-button']}
+        id={styles['confirm-button']}
+        onClick={() => handleReportConfirm(theftPosition)}>
+        Confirm
+      </button>
+    </div>
+)
 }
 
 export default ReportModal
