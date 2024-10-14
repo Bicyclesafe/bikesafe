13e8e87de442043c52abc1248fc05bc1a64fdecf
Tommi Juslin <tommi.juslin@gmail.com>
2024-10-14 12:16:39 +0300

    create buttons for google/email logins

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { auth, provider } from "../services/google_authentication"
 import { signInWithPopup, User } from "firebase/auth"
 import { Navigate } from "react-router-dom"
 
 const Login = () => {
   const [user, setUser] = useState<User | null>(null)
-
-  useEffect(() => {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      setUser(result.user)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-  }, [])
   
+  const googlePopupLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider)
+      setUser(result.user)
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  const emailLogin = async () => {
+    return
+  }
 
   return (
-    user ? <Navigate replace to="/" /> : <div>Login</div> 
+    user
+      ? <Navigate replace to="/" />
+      : <div>
+          <div>
+            Login
+          </div>
+          <button onClick={googlePopupLogin}>Google</button>
+          <br />
+          <button onClick={emailLogin}>Email</button>
+        </div>
   )
 }
 
