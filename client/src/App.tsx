77bceee50ee63c4d7ae6f627d13f43d1d928e5a5
Tommi Juslin <tommi.juslin@gmail.com>
2024-10-15 10:43:30 +0300

    use context for user state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import Login from "./components/Login"
+import AuthProvider from "./components/AuthProvider"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path={"/"} element={<HomePage />} />
-        <Route path={"/login"} element={<Login />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+          <Route path={"/login"} element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
