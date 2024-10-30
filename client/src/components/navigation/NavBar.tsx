3a4afb1404ce63ef74d8fe3fedb7a6948bd5abb1
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2024-10-30 10:58:27 +0200

    Create logout functionality

--- client/src/components/navigation/NavBar.tsx
+++ client/src/components/navigation/NavBar.tsx
@@ -1,6 +1,16 @@
 import styles from './Navbar.module.css'
+import { useAuth } from '../../hooks/useAuth'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../services/google_authentication'
 
 const NavBar = () => {
+    const { user } = useAuth()
+
+    const logOut = async () => {
+        await signOut(auth)
+        console.log("Logged out")
+    }
+
     return (
         <header className={styles['header']}>
             <div className={styles['nav-container']}>
@@ -11,7 +21,11 @@ const NavBar = () => {
                     <ul className={styles['nav-links']}>
                         <li><a href="/">Home</a></li>
                         <li><a href="/">About</a></li>
-                        <li><a href="/login">Log In</a></li>
+                        {user
+                            ? <li><button onClick={logOut}>Log out</button></li>
+                            : <li><a href="/login">Log In</a></li>
+                        }
+                        
                     </ul>
                 </nav>
             </div>
