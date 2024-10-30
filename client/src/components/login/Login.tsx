5f87d7478175e071bbce0ad0bca72cd3ddb78c99
Erik Peteri <peteri.erik@gmail.com>
2024-10-30 13:33:10 +0200

    start form styling

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -2,25 +2,25 @@ import { useState } from "react"
 import { auth, provider } from "../../services/google_authentication"
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth"
 import { Navigate } from "react-router-dom"
-import Notification from "../notification/Notification"
+//import Notification from "../notification/Notification"
 import { useAuth } from "../../hooks/useAuth"
 import stylesLogin from "../login/Login.module.css"
-import image from "../../assets/polkupyora_kuva.jpg"
+//import image from "../../assets/polkupyora_kuva.jpg"
 
 const Login = () => {
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
-  const [notification, setNotification] = useState<boolean>(false)
-  const [notificationMessage, setNotificationMessage] = useState<string>("")
+  //const [notification, setNotification] = useState<boolean>(false)
+  //const [notificationMessage, setNotificationMessage] = useState<string>("")
   const { user } = useAuth()
 
-  const createNotification = (message: string) => {
+  /*const createNotification = (message: string) => {
     setNotificationMessage(message)
     setNotification(true)
     setTimeout(() => {
       setNotification(false)  
       }, 3000)
-}
+    }*/
 
   const googlePopupLogin = async () => {
     try {
@@ -36,7 +36,7 @@ const Login = () => {
       await signInWithEmailAndPassword(auth, email, password)
     } catch (error) {
       console.error(error)
-      createNotification('Invalid user credentials')
+      //createNotification('Invalid user credentials')
     }
 
   }
@@ -49,44 +49,45 @@ const Login = () => {
     setPassword(event.currentTarget.value)
   }
 
-
-
   return (
-    user
-      ? <Navigate replace to="/" />
-      : <div className={stylesLogin['login-container']}>
-          <div className={stylesLogin['content-container']}>
-            <Notification visible={notification} message={notificationMessage} />
-            <div className={stylesLogin['column-left']}>
-              <div className={stylesLogin['login form']}>
-              <form className={stylesLogin['login-form']} onSubmit={loginUser}>
-                <label>email</label>
-                <input
-                  id="email"
-                  value={email}
-                  onChange={handleEmailChange}
+    user 
+    ? <Navigate replace to="/" />
+    :
+    <div className={stylesLogin['login-container']}>
+      <div className={stylesLogin['content-container']}>
+        <div className={stylesLogin['column-left']}>
+          <div className={stylesLogin.form}>
+            <header>Login</header>
+            <form onSubmit={loginUser}>
+              <input
+                id="email"
+                value={email} type="text"
+                onChange={handleEmailChange}
+                placeholder="Enter your email"
                 />
-              
-                
-                <label>password</label>
-                <input
-                  id="password"
-                  type="password"
-                  value={password}
-                  onChange={handlePasswordChange}
+              <input id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                type="password" placeholder="Enter your password" 
                 />
-                <button id="login-button" type="submit">Login</button>
-              </form>
-              <button onClick={googlePopupLogin}>Google</button>
-              </div>
-            </div>
-            <div className={stylesLogin['column-right']}>
-              <img src={image}/>
+              <a href="#">Forgot password?</a>
+              <button id='login-button' type="submit" className={stylesLogin.button}>Login</button>
+            </form>
+            <button onClick={googlePopupLogin}>Google</button>
+            <div className={stylesLogin.signup}>
+              <span>
+                Don't have an account?
+                <a href="/register">
+                  <label htmlFor="check">Signup</label>
+                </a>
+              </span>
             </div>
           </div>
         </div>
-       
-  )
-}
+        <div className={stylesLogin['column-right']}>
+        </div>
+      </div>
+    </div>
+  )}
 
 export default Login
