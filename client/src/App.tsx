329487a564cbbafa02bb16996bce1384efd5a0d8
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2024-10-30 12:21:15 +0200

    Fix lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
         <Router>
           <NavBar/>
           <Routes>
-            <Route path={"/login"} element={<Login />} /
+            <Route path={"/login"} element={<Login />} />
 
             <Route element={<AuthWrapper />}>
               <Route path={"/"} element={<HomePage />} />
