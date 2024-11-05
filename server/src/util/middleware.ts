476c7d6c25ce7c3753c4f97caeae304e42dfc7a6
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2024-11-05 13:42:50 +0200

    Create tests for userRoute and SeasonalDistance

--- server/src/util/middleware.ts
+++ server/src/util/middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 
 export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  console.error(err.message)
-
+  console.log(err.message)
   res.status(500).send({ errors: [{ message: "Something went wrong" }] })
 }
