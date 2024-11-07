1e9f3deac1cc45e78a6428e37ab3ce5f6fe4494e
Tommi Juslin <tommi.juslin@gmail.com>
2024-11-07 17:31:05 +0200

    Add a button for adding a goal if no current goals

--- client/src/components/dashboard/PersonalGoalTracker.tsx
+++ client/src/components/dashboard/PersonalGoalTracker.tsx
@@ -6,42 +6,43 @@ import tripService from "../../services/tripService"
 
 const PersonalGoalTracker = () => {
   const [currentGoals, setCurrentGoals] = useState<PersonalGoal[]>([])
-  const [currentProgress, setCurrentProgress] = useState<number | null>(null)
+  const [currentProgress, setCurrentProgress] = useState<number>(0)
   const { user } = useAuth()
   
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchGoals = async () => {
       const currentGoalResponse = await goalService.getCurrentGoalsForUser(user?.uid)
       setCurrentGoals(currentGoalResponse)
     }
-    fetchData()
-  }, [])
+    fetchGoals()
+  }, [user?.uid])
 
   useEffect(() => {
     const fetchProgress = async () => {
       if (currentGoals.length > 0 && user?.uid) {
-        console.log(currentGoals[0].startTime);
         const tripsResponse = await tripService.getTripsBetweenDates(
           user.uid,
           currentGoals[0].startTime,
           currentGoals[0].endTime
-        );
-        setCurrentProgress(tripsResponse);
+        )
+        setCurrentProgress(tripsResponse || 0)
       }
-    };
-    fetchProgress();
-  }, [currentGoals])
+    }
+    fetchProgress()
+  }, [currentGoals, user?.uid])
   
   return(
-    currentGoals.length > 0 && currentProgress
-      ?
-        <>
-          {currentProgress || 0} / {currentGoals[0].goalDistance}
-          <progress value={currentProgress/currentGoals[0].goalDistance}/>
-        </>
-      :
-        <div>Loading...</div>
+    currentGoals.length > 0 ? (
+      <>
+        {currentProgress || 0} / {currentGoals[0].goalDistance}
+        <progress value={currentProgress/currentGoals[0].goalDistance}/>
+      </>
+    ) : (
+      <>
+        No current goals. <button onClick={() => {}}>Set a Goal</button>
+      </>   
+    )
   )
 }
 
