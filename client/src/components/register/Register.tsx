9a23243604082794c27a2a6e78ae9355a89b41cb
Erik Peteri <peteri.erik@gmail.com>
2024-11-08 12:17:23 +0200

    style, validate and add error messages to registration form

--- client/src/components/register/Register.tsx
+++ client/src/components/register/Register.tsx
@@ -1,45 +1,63 @@
 import { useState } from "react"
 import { auth } from "../../services/google_authentication"
 import { createUserWithEmailAndPassword } from "firebase/auth"
-import Notification from "../notification/Notification"
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../../hooks/useAuth"
+import styles from './Register.module.css'
+import { FirebaseError } from "firebase/app" 
+
 
 const Register = () => {
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
   const [passwordConfirm, setPasswordConfirm] = useState<string>("")
-  const [notification, setNotification] = useState<boolean>(false)
-  const [notificationMessage, setNotificationMessage] = useState<string>("")
+  const [errors, setErrors] = useState<{ [key: string]: string }>({})
   const { user } = useAuth()
-
-  const createNotification = (message: string) => {
-    setNotificationMessage(message)
-    setNotification(true)
-    setTimeout(() => {
-      setNotification(false)  
-      }, 3000)
-  }
 
   const registerUser = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    const fieldErrors: { [key: string]: string } = {}
+    let formIsValid = true
+
+    if (password !== passwordConfirm) {
+      fieldErrors.passwordConfirm = 'Passwords dont match'
+      formIsValid = false
+      }
+
+    if (password.length < 6) {
+      fieldErrors.password = 'Password must be at least 6 characters long'
+      formIsValid = false
+    }
+
+    if (!email) {
+      fieldErrors.email = 'Email is required'
+      formIsValid = false
+    }
+
+    if (!passwordConfirm) {
+      fieldErrors.passwordConfirm = 'Password confirmation is required'
+      formIsValid = false
+    }
+
+    if (!formIsValid) {
+      setErrors(fieldErrors)
+      return
+    }
+
     try {
-      if (password !== passwordConfirm) {
-        createNotification('Password and password confirmation do not match')
-        return 
-      }
-      
-      else if (password.length < 6) {
-        createNotification('Minimum length for password is 6 characters')
-        return
-      }
-
       await createUserWithEmailAndPassword(auth, email, password)
-      createNotification('User has been created!')
-
-    } catch(error) {
-      console.error(error)
-      createNotification('Make sure you are using a correctly formatted email and password')
+      console.log('Account created successfully!')
+    } catch(error: unknown) {
+      if (error instanceof FirebaseError) {
+        if (error.code === 'auth/email-already-in-use') {
+          fieldErrors.email = 'Email is already in use'
+        } else {
+          fieldErrors.general = 'An unexpected error occurred'
+        }
+      } else {
+        fieldErrors.general = 'An unexpected error occurred, please try again.'
+      }
+      setErrors(fieldErrors)
     }
   }
 
@@ -60,34 +78,58 @@ const Register = () => {
       ? 
       <Navigate replace to="/login" />
       :  
-      <div>
-        <Notification visible={notification} message={notificationMessage}/>
-        <h1>Register</h1>
-        <form onSubmit={registerUser}>
-          email
-          <input
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          password
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          password confirmation
-          <input
-            id="passwordConfirm"
-            type="password"
-            value={passwordConfirm}
-            onChange={handlePasswordConfirmChange}
-          />
-          <button type="submit" id="register-button">Register</button>
-        </form>
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <h1>Sign up</h1>
+          {errors.general && <div className={styles.errorGeneral}>{errors.general}</div>}
+          <form onSubmit={registerUser}>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Email</label>
+              <span>*</span>
+              <input
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                className={errors.email ? styles.errorInput : ''}
+              />
+              {errors.email && <span className={styles.errorMessage}>{errors.email}</span>}
+            </div>
+            
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">Password</label>
+              <span>*</span>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className={errors.password ? styles.errorInput : ''}
+              />
+              {errors.password && <span className={styles.errorMessage}>{errors.password}</span>}
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label htmlFor="passwordConfirm">Password Confirmation</label>
+              <span>*</span>
+              <input
+                id="passwordConfirm"
+                type="password"
+                value={passwordConfirm}
+                onChange={handlePasswordConfirmChange}
+                className={errors.passwordConfirm ? styles.errorInput : ''}
+              />
+              {errors.passwordConfirm && <span className={styles.errorMessage}>{errors.passwordConfirm}</span>}
+            </div>
+
+            <button type="submit" id="register-button">Sign up</button>
+          </form>
+
+          <div className={styles.login}>
+            <p>Already have an account? <a href="/login">Log In</a></p>
+          </div>
+        </div>
       </div>
   )
 }
-
 export default Register
