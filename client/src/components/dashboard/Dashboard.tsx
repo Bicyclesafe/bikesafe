a33209e80a34baaed436a528de9d529bd13fdc03
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2024-11-08 17:40:33 +0200

    Fix client tests

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,6 @@
-import { useAuth } from "../../hooks/useAuth"
 import SeasonalDistance from "./SeasonalDistance"
 
 const Dashboard = () => {
-  const { user } = useAuth()
-
-  const sendToken = async () => {
-    const token = await user?.getIdToken(true)
-    console.log(token)
-  }
-  sendToken()
   return (
     <div>
       <h1>Dashboard</h1>
