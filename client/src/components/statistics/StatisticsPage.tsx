4033aab4808d68b05a02af0e59fd50707f09a7e5
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-11-25 11:21:10 +0200

    style statistics page

--- client/src/components/statistics/StatisticsPage.tsx
+++ client/src/components/statistics/StatisticsPage.tsx
@@ -1,11 +1,16 @@
 import DistanceBarChart from "./DistanceBarChart"
+import stylesStatistics from "./StatisticsPage.module.css"
 
 const StatisticsPage = () => {
-    return (
-        <div>
+  return (
+    <div className={stylesStatistics['statistics-container']}>
+      <div className={stylesStatistics['statistics-content']}>
+        <div className={stylesStatistics['chart']}>
             <DistanceBarChart/>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default StatisticsPage
