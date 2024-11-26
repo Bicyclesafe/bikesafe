7c1e52f60350ffccf275979e3da5dc24bc34a48a
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-11-26 09:49:11 +0200

    Refactor latestTrips to own component

--- client/src/components/statistics/StatisticsPage.tsx
+++ client/src/components/statistics/StatisticsPage.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import DistanceBarChart from "./DistanceBarChart"
 import stylesStatistics from "./StatisticsPage.module.css"
 import tripService from "../../services/tripService"
 import { useAuth } from "../../hooks/useAuth"
 import { Trip } from "../../types"
-import { Duration, format, intervalToDuration } from "date-fns"
+import LatestTrips from "./LatestTrips"
 
 const StatisticsPage = () => {
   const { user } = useAuth()
@@ -27,68 +27,9 @@ const StatisticsPage = () => {
     fetchData()
   }, [user])
 
-  const formatDuration = (duration: Duration) => {
-    const { hours = 0, minutes = 0 } = duration
-  
-    if (hours === 0) {
-      return (
-        <>
-          <span className={stylesStatistics['duration']}>{minutes}</span>
-          <span className={stylesStatistics['unit']}>min</span>
-        </>
-      )
-    }
 
-    if (minutes === 0) {
-      return (
-        <>
-          <span className={stylesStatistics['duration']}>{hours}</span>
-          <span className={stylesStatistics['unit']}>h</span>
-        </>
-      )
-    }
-    return (
-      <>
-        <span className={stylesStatistics['duration']}>{hours}</span>
-        <span className={stylesStatistics['unit']}>h</span>
-        <span style={{ marginLeft: "5px" }}>
-          <span className={stylesStatistics['duration']}>{minutes}</span>
-          <span className={stylesStatistics['unit']}>m</span>
-        </span>
-      </>
-    )
-  }
 
-  const latestTrips = useMemo(() => {
-    return rawData
-      .sort((a, b) => new Date(b.startTime).getTime() - new Date(a.startTime).getTime())
-      .slice(0, 5)
-  }, [rawData])
 
-  const showLatestTrips = latestTrips.map((trip) => {
-    const startTime = new Date(trip.startTime)
-    const duration = intervalToDuration({ start: trip.startTime, end: trip.endTime })
-
-    return (
-      <div className={stylesStatistics['trip-box']} key={trip.id}>
-        <div className={stylesStatistics['date']}>{format(startTime, 'dd MMM yyyy')}</div>
-        <div className={stylesStatistics['start-time']}>{format(startTime, 'HH:mm')}</div>
-        <div className={stylesStatistics['details']}>
-          <div>
-            <span>{formatDuration(duration)}</span>
-          </div>
-          <div>
-            <span className={stylesStatistics['distance']}>{Number(trip.tripDistance.toFixed(1))}</span>
-            <span className={stylesStatistics['unit']}>km</span>
-          </div>
-          <div>
-            <span className={stylesStatistics['co2']}>{((140 * trip.tripDistance) / 100).toFixed(0)}</span>
-            <span className={stylesStatistics['unit']}>CO<sub>2</sub></span>
-          </div>
-        </div>
-      </div>
-    )
-  })
 
   const yearlyTotalDistance = () => {
     // const filteredTrips = rawData.filter(
@@ -122,7 +63,7 @@ const StatisticsPage = () => {
           <div className={stylesStatistics['trip-container-title']}>
             Latest trips
           </div>
-          {showLatestTrips}
+          <LatestTrips rawData={rawData} />
         </div>
       </div>
     </div>
