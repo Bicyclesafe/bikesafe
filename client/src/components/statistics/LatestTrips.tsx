d53b15e80e4fbe0fa966e4d26777df605e85f5ee
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-11-27 12:36:20 +0200

    make formatDuration its own component

--- client/src/components/statistics/LatestTrips.tsx
+++ client/src/components/statistics/LatestTrips.tsx
@@ -3,39 +3,39 @@ import stylesStatistics from "../statistics/StatisticsPage.module.css"
 import { FC, useMemo } from "react"
 import { Trip } from "../../types"
 
-const LatestTrips : FC<{ rawData: Trip[]}> = ({ rawData = []})=> {
-  const formatDuration = (duration: Duration) => {
-    const { hours = 0, minutes = 0 } = duration
-  
-    if (hours === 0) {
-      return (
-        <>
-          <span className={stylesStatistics['duration']}>{minutes}</span>
-          <span className={stylesStatistics['unit']}>min</span>
-        </>
-      )
-    }
+export const StyledDuration: FC<{ duration: Duration }> = ({ duration }) => {
+  const { hours = 0, minutes = 0 } = duration
 
-    if (minutes === 0) {
-      return (
-        <>
-          <span className={stylesStatistics['duration']}>{hours}</span>
-          <span className={stylesStatistics['unit']}>h</span>
-        </>
-      )
-    }
+  if (hours === 0) {
     return (
       <>
-        <span className={stylesStatistics['duration']}>{hours}</span>
-        <span className={stylesStatistics['unit']}>h</span>
-        <span style={{ marginLeft: "5px" }}>
-          <span className={stylesStatistics['duration']}>{minutes}</span>
-          <span className={stylesStatistics['unit']}>m</span>
-        </span>
+        <span className={stylesStatistics['duration']}>{minutes}</span>
+        <span className={stylesStatistics['unit']}>min</span>
       </>
     )
   }
 
+  if (minutes === 0) {
+    return (
+      <>
+        <span className={stylesStatistics['duration']}>{hours}</span>
+        <span className={stylesStatistics['unit']}>h</span>
+      </>
+    )
+  }
+  return (
+    <>
+      <span className={stylesStatistics['duration']}>{hours}</span>
+      <span className={stylesStatistics['unit']}>h</span>
+      <span style={{ marginLeft: "5px" }}>
+        <span className={stylesStatistics['duration']}>{minutes}</span>
+        <span className={stylesStatistics['unit']}>m</span>
+      </span>
+    </>
+  )
+}
+
+const LatestTrips : FC<{ rawData: Trip[]}> = ({ rawData })=> {
   const latestTrips = useMemo(() => {
       return rawData
         .sort((a, b) => new Date(b.startTime).getTime() - new Date(a.startTime).getTime())
@@ -52,15 +52,15 @@ const LatestTrips : FC<{ rawData: Trip[]}> = ({ rawData = []})=> {
     <div className={stylesStatistics['start-time']}>{format(startTime, 'HH:mm')}</div>
     <div className={stylesStatistics['details']}>
         <div>
-        <span>{formatDuration(duration)}</span>
+          <span><StyledDuration duration={duration} /></span>
         </div>
         <div>
-        <span className={stylesStatistics['distance']}>{Number(trip.tripDistance.toFixed(1))}</span>
-        <span className={stylesStatistics['unit']}>km</span>
+          <span className={stylesStatistics['distance']}>{Number(trip.tripDistance.toFixed(1))}</span>
+          <span className={stylesStatistics['unit']}>km</span>
         </div>
         <div>
-        <span className={stylesStatistics['co2']}>{((140 * trip.tripDistance) / 100).toFixed(0)}</span>
-        <span className={stylesStatistics['unit']}>CO<sub>2</sub> (kg)</span>
+          <span className={stylesStatistics['co2']}>{((258 * trip.tripDistance) / 1000).toFixed(0)}</span>
+          <span className={stylesStatistics['unit']}>CO<sub>2</sub> (kg)</span>
         </div>
     </div>
     </div>
