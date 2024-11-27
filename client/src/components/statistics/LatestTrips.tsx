ad71b9b9fe27d382f10fbea971a2496dd22f873c
tommijuslin <tommi.juslin@gmail.com>
2024-11-27 21:14:45 +0200

    move styling for LatestTrips into its own file

--- client/src/components/statistics/LatestTrips.tsx
+++ client/src/components/statistics/LatestTrips.tsx
@@ -1,5 +1,5 @@
 import { Duration, format, intervalToDuration } from "date-fns"
-import stylesStatistics from "../statistics/StatisticsPage.module.css"
+import stylesLatestTrips from "../statistics/LatestTrips.module.css"
 import { FC, useMemo } from "react"
 import { Trip } from "../../types"
 
@@ -9,8 +9,8 @@ export const StyledDuration: FC<{ duration: Duration }> = ({ duration }) => {
   if (hours === 0) {
     return (
       <>
-        <span className={stylesStatistics['duration']}>{minutes}</span>
-        <span className={stylesStatistics['unit']}>min</span>
+        <span className={stylesLatestTrips['duration']}>{minutes}</span>
+        <span className={stylesLatestTrips['unit']}>min</span>
       </>
     )
   }
@@ -18,18 +18,18 @@ export const StyledDuration: FC<{ duration: Duration }> = ({ duration }) => {
   if (minutes === 0) {
     return (
       <>
-        <span className={stylesStatistics['duration']}>{hours}</span>
-        <span className={stylesStatistics['unit']}>h</span>
+        <span className={stylesLatestTrips['duration']}>{hours}</span>
+        <span className={stylesLatestTrips['unit']}>h</span>
       </>
     )
   }
   return (
     <>
-      <span className={stylesStatistics['duration']}>{hours}</span>
-      <span className={stylesStatistics['unit']}>h</span>
+      <span className={stylesLatestTrips['duration']}>{hours}</span>
+      <span className={stylesLatestTrips['unit']}>h</span>
       <span style={{ marginLeft: "5px" }}>
-        <span className={stylesStatistics['duration']}>{minutes}</span>
-        <span className={stylesStatistics['unit']}>m</span>
+        <span className={stylesLatestTrips['duration']}>{minutes}</span>
+        <span className={stylesLatestTrips['unit']}>m</span>
       </span>
     </>
   )
@@ -47,21 +47,21 @@ const LatestTrips : FC<{ rawData: Trip[]}> = ({ rawData })=> {
   const duration = intervalToDuration({ start: trip.startTime, end: trip.endTime })
 
   return (
-    <div className={stylesStatistics['trip-box']} key={trip.id}>
-    <div className={stylesStatistics['date']}>{format(startTime, 'dd MMM yyyy')}</div>
-    <div className={stylesStatistics['start-time']}>{format(startTime, 'HH:mm')}</div>
-    <div className={stylesStatistics['details']}>
-        <div>
-          <span><StyledDuration duration={duration} /></span>
-        </div>
-        <div>
-          <span className={stylesStatistics['distance']}>{Number(trip.tripDistance.toFixed(1))}</span>
-          <span className={stylesStatistics['unit']}>km</span>
-        </div>
-        <div>
-          <span className={stylesStatistics['co2']}>{((258 * trip.tripDistance) / 1000).toFixed(0)}</span>
-          <span className={stylesStatistics['unit']}>CO<sub>2</sub> (kg)</span>
-        </div>
+    <div className={stylesLatestTrips['trip-box']} key={trip.id}>
+    <div className={stylesLatestTrips['date']}>{format(startTime, 'dd MMM yyyy')}</div>
+    <div className={stylesLatestTrips['start-time']}>{format(startTime, 'HH:mm')}</div>
+    <div className={stylesLatestTrips['details']}>
+      <div>
+        <StyledDuration duration={duration} />
+      </div>
+      <div>
+        <span className={stylesLatestTrips['distance']}>{Number(trip.tripDistance.toFixed(1))}</span>
+        <span className={stylesLatestTrips['unit']}>km</span>
+      </div>
+      <div>
+        <span className={stylesLatestTrips['co2']}>{((258 * trip.tripDistance) / 1000).toFixed(0)}</span>
+        <span className={stylesLatestTrips['unit']}>CO<sub>2</sub> (kg)</span>
+      </div>
     </div>
     </div>
   )
